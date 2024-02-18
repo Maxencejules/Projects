b24fa38551d8df9ff7805856a13db9485ca1990d
Maxence Jules <142577996+Maxencejules@users.noreply.github.com>
2024-02-17 22:29:59 -0500

    Create intitals.py

---
+++ intitals.py
@@ -0,0 +1,9 @@
+#Maxence Jules
+# Fun fact: I made this program
+print("M       M    J J J J J")
+print("MM     MM        J")
+print("MM     MM        J")
+print("M   M   M        J")
+print("M       M  J     J")
+print("M       M  J     J")
+print("M       M     J  J")
